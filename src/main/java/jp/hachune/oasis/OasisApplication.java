0dea46e17d05b1c2e1a79921fc4dd33cd29c2f71
letsspeak <letsinfinite@gmail.com>
2017-03-25 14:01:05 +0900

    first commit

---
+++ src/main/java/jp/hachune/oasis/OasisApplication.java
@@ -0,0 +1,12 @@
+package jp.hachune.oasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OasisApplication.class, args);
+	}
+}
